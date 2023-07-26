262cb4a36cc9baf2d6530e2c7ce9e180061e5f7a
hobiJeong <jjb26433@naver.com>
2023-07-26 15:20:30 +0900

    post기능 id값 출력 작업중

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 
-const posttodo = require("./todoController");
+const { posttodo,postget,postoneget } = require("./todoController");
 
 router.get("/", (req, res) => {
     res.send("화이팅");
@@ -15,6 +15,8 @@ router.get("/post", (req,res) => {
 });
 
 router.post("/post", posttodo);
+router.get("/get", postget);
+router.get("/get", postoneget);
 
 
 
--- src/todoController.js
+++ src/todoController.js
@@ -4,19 +4,44 @@ const db = require("./config/db");
     const posttodo = (req, res) => {
     const query = "INSERT INTO posts SET ?";
 
-    const { id, description } = req.body;
+    const { title, description, target_date } = req.body;
 
-    db.query(query, { id, description },
+    db.query(query, { title, description, target_date },
         (err, result) => {
+            const post = {
+                "title": title,
+                "description": description,
+                "target_date": target_date,
+            };
             if (err) return res.json(err);
-            result = { 
-                "id": id,
-                "description" : description
-            }
-            return res.status(200).json(result);
+            return res.status(201).json(post);
         });
     }
 
-module.exports = posttodo;
+    const postget = (req, res) => {
+    const query = "SELECT * FROM posts";
+    
+    db.query(query,(err,result) => {
+        if (err) return res.json(err);
+        return res.status(200).json(result);
+    });
+    }
 
+    const postoneget = (req, res) => {
+    const query = "SELECT ? FROM posts";
+
+    const id = req.body;
+    db.query(query,id, (err,result) => {
+        if (err) return res.json(err);
+        return res.status(200).json(result);
+    })
+    }
+
+
+
+module.exports = {
+    posttodo,
+    postget,
+    postoneget,
+};
 
