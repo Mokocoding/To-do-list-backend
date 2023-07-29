cca33da15d3e2248150851748656fe134c76e201
hobiJeong <jjb26433@naver.com>
2023-07-29 20:16:38 +0900

    biho : CRUD기능 전체 리팩토링 및 디버깅용 console.log 삭제

--- src/models/CRUD.js
+++ src/models/CRUD.js
@@ -8,12 +8,64 @@ class CRUD {
     }
 
     async post() {
+
         const post = this.body;
+
         try {
             const response = await PostCRUD.postcreate(post); 
-            console.log(response);
             return response;
-        }   catch (err) {
+        }   
+        catch (err) {
+            return { success: false, msg: err };
+        }
+    }
+
+    async get() {
+
+        try {
+            const response = await PostCRUD.postget();
+            return response;
+        }   
+        catch (err) {
+            return { success: false, msg: err };
+        }
+    }
+
+    async oneget() {
+
+        const post = this.body;
+
+        try {
+            const response = await PostCRUD.postoneget(post);
+            return response;
+        } 
+        catch (err) {
+            return { success: false, msg: err };
+        }
+    }
+
+    async update() {
+
+        const post = this.body;
+
+        try {
+            const response = await PostCRUD.postupdate(post);
+            return response;
+        } 
+        catch (err) {
+            return { success: false, msg: err };
+        }
+    }
+
+    async delete() {
+
+        const post = this.body;
+
+        try {
+            const response = await PostCRUD.postdelete(post);
+            return response;
+        } 
+        catch (err) {
             return { success: false, msg: err };
         }
     }
--- src/todoController.js
+++ src/todoController.js
@@ -1,77 +1,36 @@
 "use strict";
-const db = require("./config/db");
-const PostCRUD = require("./models/PostCRUD");
 const CRUD = require("./models/CRUD");
 
     const posttodo = async (req, res) => {
         const newPost = new CRUD(req.body); 
         const response = await newPost.post()
-        return res.json(response);
-        // db.query(query, { description, target_date },
-        //     (err, result) => {
-        //         const post = {
-        //             "id": result.insertId,
-        //             "description": description,
-        //             "target_date": target_date,
-        //         };
-        //         if (err) return res.json(err);
-        //         return res.status(201).json(post);
-            // }); 
+        return res.status(201).json(response);
     } 
 
 
-    const postget = (req, res) => {
-        const query = "SELECT * FROM posts";
-        
-        db.query(query,(err,result) => {
-            if (err) return res.json(err);
-            return res.status(200).json(result);
-        });
+    const postget = async (req, res) => {
+        const newPost = new CRUD();
+        const response = await newPost.get();
+        return res.status(200).json(response);
     }
 
-    const postoneget = (req, res) => {
-        const query = `SELECT * FROM posts where id=${req.params.id}`;
-
-        db.query(query,(err,result) => {
-            if (err) return res.json(err);
-            return res.status(200).json(result);
-        });
+    const postoneget = async (req, res) => {
+        const newPost = new CRUD(req.params.id);
+        const response = await newPost.oneget();
+        return res.status(200).json(response);
     }
 
-    const postpatch = (req, res) => {
-        const query = `UPDATE posts SET ? where id=${req.params.id}`;
-        const { description, target_date } = req.body;
-
-        db.query(query,{ description, target_date}, 
-            (err, result) => {
-                const post = {
-                    "id": req.params.id,
-                    "description": description,
-                    "target_date": target_date
-                };
-                if(err) return res.json(err);
-                return res.status(200).json(post);
-            });
+    const postpatch = async (req, res) => {
+        const newPost = new CRUD(req);
+        const response = await newPost.update();
+        return res.status(200).json(response);
     }
 
-    const postdelete = (req, res) => {
-        const query = `DELETE FROM posts WHERE id=${req.params.id}`;
-
-        db.query(query,(err, result) => {
-            if(err) return res.json(err);
-            return res.status(200).json("삭제완료");
-        });
+    const postdelete = async (req, res) => {
+        const newPost = new CRUD(req.params.id);
+        const response = await newPost.delete();
+        return res.json(response);
     }
-    // const postoneget = (req, res) => {
-    // const query = "SELECT ? FROM posts";
-
-    // const id = req.body;
-    // db.query(query,id, (err,result) => {
-    //     if (err) return res.json(err);
-    //     return res.status(200).json(result);
-    // })
-    // }
-
 
 
 module.exports = {
