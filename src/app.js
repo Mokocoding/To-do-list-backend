799e5da3cde575fd447a6fc480da8dda9d6516e0
hobiJeong <jjb26433@naver.com>
2023-07-31 15:01:00 +0900

    오리진

--- src/app.js
+++ src/app.js
@@ -11,21 +11,21 @@ const home = require("./");
 
 app.use(cors()); // Use this after the variable declaration
 
-// app.use(function(req, res, next)  {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Headers", "*");
-//     res.header("Access-Control-Allow-Credential: true");
-//     res.header("ngrok-skip-browser-warning: true");
-//     next();
-// });
+app.use(function(req, res, next)  {
+    res.header("Access-Control-Allow-Origin", "ec2-3-35-134-247.ap-northeast-2.compute.amazonaws.com:3000/");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Credential: true");
+    res.header("ngrok-skip-browser-warning: true");
+    next();
+});
 
-// const corsOptions ={
-//     origin: "http://localhost:3000",
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     preflightContinue: true,
-//     optionsSuccessStatus: 200
-//   }
-// app.use(cors(corsOptions));
+const corsOptions ={
+    origin: "ec2-3-35-134-247.ap-northeast-2.compute.amazonaws.com:3000/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: true,
+    optionsSuccessStatus: 200
+  }
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
